c5b79d7187850a73c57ed90459cc4b4c779dd5eb
DESKTOP-293EMP1\jonjh <haase1020@gmail.com>
2020-09-17 15:31:35 -0400

    trying to fix bug

--- script.js
+++ script.js
@@ -5,23 +5,23 @@ const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
 const loader = document.getElementById('loader');
 
-//show loading
-// function loading() {
-//   loader.hidden = false;
-//   quoteContainer.hidden = true;
-// }
+// show loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
 
 // hide loading
-// function complete() {
-//   if (!loader.hidden) {
-//     quoteContainer.hidden = false;
-//     loader.hidden = true;
-//   }
-// }
+function complete() {
+  if (!loader.hidden) {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+  }
+}
 
 // get quote from API
 async function getQuote() {
-  //   loading();
+  loading();
   const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
   const apiUrl =
     'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
@@ -44,9 +44,10 @@ async function getQuote() {
     }
     quoteText.innerText = data.quoteText;
     // stop loader, show quote
-    // complete();
+    complete();
   } catch (error) {
-    getQuote(); //due to API issues
+    // getQuote(); //due to API issues
+    console.log('you have a problem here');
   }
 }
 
